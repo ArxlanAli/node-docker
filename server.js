d7e124434d2eee57a8329d7c6313536df6d04285
Arslan Ali <arslan.ali@ebryx.com>
2020-08-12 20:00:57 +0500

    Ninth commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello from docker jenkins 8');
+    res.send('Hello from docker jenkins 9');
 });
 
 app.listen('3000',() => console.log('server is listeinign'));
