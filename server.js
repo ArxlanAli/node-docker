9842807ca5ac109143348540f1e15cc502d5625c
Arslan Ali <arslan.ali@ebryx.com>
2020-08-12 16:34:56 +0500

    Fourth commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello from docker jenkins 3');
+    res.send('Hello from docker jenkins 4');
 });
 
 app.listen('3000',() => console.log('server is listeinign'));
