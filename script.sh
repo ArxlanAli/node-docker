eb518617cbf6a851f44596d7da3de4eb732d0f59
Arslan Ali <arslan.ali@ebryx.com>
2020-08-13 18:07:08 +0500

    fixes

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir /var/local/installers
+# mkdir /var/local/installers
 
-# if [ $(sudo docker images -f "dangling=true" -q) ]; then
-#   echo "Found a Dangling images"
-#    sudo docker rmi $(sudo docker images -f "dangling=true" -q)
-# else
-#   echo "not found any dangling image"
-# fi
+if [ $(sudo docker images -f "dangling=true" -q) ]; then
+  echo "Found a Dangling images"
+   sudo docker rmi $(sudo docker images -f "dangling=true" -q)
+else
+  echo "not found any dangling image"
+fi
 
