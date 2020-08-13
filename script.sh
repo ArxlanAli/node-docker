89c0b06869d35eb2e0e382ee291b03a9fca26387
Arslan Ali <arslan.ali@ebryx.com>
2020-08-13 16:02:26 +0500

    Update in docker file

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ $(sudo docker images -f "dangling=true" -q) ]; then
-  echo "Found a Dangling images"
-   sudo docker rmi $(sudo docker images -f "dangling=true" -q)
-else
-  echo "not found any dangling image"
-fi
+mkdir /var/local/installers
+
+# if [ $(sudo docker images -f "dangling=true" -q) ]; then
+#   echo "Found a Dangling images"
+#    sudo docker rmi $(sudo docker images -f "dangling=true" -q)
+# else
+#   echo "not found any dangling image"
+# fi
 
